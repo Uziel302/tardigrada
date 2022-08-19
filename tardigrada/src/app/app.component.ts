8c771d397479f276287addcc15549e3e44e362fd
Asaf M <302302302v@gmail.com>
2022-08-19 14:18:39 +0300

    create header and footer

--- tardigrada/src/app/app.component.ts
+++ tardigrada/src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
-import { Router } from "@angular/router";
-
-
-import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -14,58 +8,6 @@ import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 export class AppComponent {
   public title = 'tardigrada';
 
-  private subscriptions: Subscription[] = [];
+  constructor(){}
 
-  constructor(    
-    public dialog: MatDialog,
-    private router: Router,
-  ){}
-
-  public openStudentLoginDialog():void{
-    const dialogRef: MatDialogRef<LoginDialogComponent, any> =
-      this.dialog.open(LoginDialogComponent, {
-        data: {type: 'student', login: true},
-      });
-      this.subscriptions.push(
-        dialogRef.afterClosed().subscribe((res:any) => {
-          this.router.navigate(['/student', res[0]]);
-        })
-      );
-  }
-
-  public openTeacherLoginDialog():void{
-    const dialogRef: MatDialogRef<LoginDialogComponent, any> =
-      this.dialog.open(LoginDialogComponent, {
-        data: {type: 'teacher', login: true},
-      });
-      this.subscriptions.push(
-        dialogRef.afterClosed().subscribe((res:any) => {
-          this.router.navigate(['/teacher', res[0]]);
-        })
-      );
-  }
-
-  public openStudentRegDialog():void{
-    const dialogRef: MatDialogRef<LoginDialogComponent, any> =
-      this.dialog.open(LoginDialogComponent, {
-        data: {type: 'student', login: false},
-      });
-      this.subscriptions.push(
-        dialogRef.afterClosed().subscribe((res:any) => {
-          this.router.navigate(['/student', res[0]]);
-        })
-      );
-  }
-
-  public openTeacherRegDialog():void{
-    const dialogRef: MatDialogRef<LoginDialogComponent, any> =
-      this.dialog.open(LoginDialogComponent, {
-        data: {type: 'teacher', login: false},
-      });
-      this.subscriptions.push(
-        dialogRef.afterClosed().subscribe((res:any) => {
-          this.router.navigate(['/teacher', res[0]]);
-        })
-      );
-  }
 }
--- tardigrada/src/app/app.module.ts
+++ tardigrada/src/app/app.module.ts
@@ -10,13 +10,17 @@ import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TeacherAreaComponent } from './teacher-area/teacher-area.component';
 import { StudentAreaComponent } from './student-area/student-area.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginDialogComponent,
     TeacherAreaComponent,
-    StudentAreaComponent
+    StudentAreaComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
