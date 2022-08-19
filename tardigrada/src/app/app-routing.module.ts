e17bc0aea6ffef9a0516020ab6fa41e1c939a115
Asaf M <302302302v@gmail.com>
2022-08-19 13:33:32 +0300

    route to new components

--- tardigrada/src/app/app-routing.module.ts
+++ tardigrada/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { StudentAreaComponent } from './student-area/student-area.component';
+import { TeacherAreaComponent } from './teacher-area/teacher-area.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'app-root', pathMatch: 'full' },
+  { path: 'student/:name', component: StudentAreaComponent },
+  { path: 'teacher/:name', component: TeacherAreaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
