456171da2c38d09c48c655d18ae0b86505f457f6
Asaf M <302302302v@gmail.com>
2022-08-25 15:14:16 +0300

    selected lecture

--- tardigrada/src/app/schedule/user-schedule-lecture/user-schedule-lecture.component.ts
+++ tardigrada/src/app/schedule/user-schedule-lecture/user-schedule-lecture.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { ILecture } from 'src/app/models/lecture';
+import { ScheduleService } from '../schedule.service';
 
 @Component({
   selector: 'app-user-schedule-lecture',
   templateUrl: './user-schedule-lecture.component.html',
-  styleUrls: ['./user-schedule-lecture.component.css']
+  styleUrls: ['./user-schedule-lecture.component.css'],
 })
 export class UserScheduleLectureComponent implements OnInit {
+  @Input() data: ILecture = {
+    title: '',
+    subtitle: '',
+    teacher: '',
+    hour: '',
+    minAge: '',
+    maxAge: '',
+    background: '',
+    url: '',
+  };
 
-  @Input() data: ILecture = {title:'',subtitle:'',teacher:'',hour:'',minAge:'',maxAge:'',background:'',url:''};
+  constructor(public scheduleService: ScheduleService) {}
 
-  constructor() {
+  ngOnInit(): void {}
+
+  selectLecture() {
+    this.scheduleService.selectedLecture = this.data;
   }
-
-  ngOnInit(): void {
-  }
-
 }
