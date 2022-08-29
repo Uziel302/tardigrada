2dbb1f35e0a0b5ef61f17e802953756161bf86de
Asaf M <302302302v@gmail.com>
2022-08-29 23:08:18 +0300

    validate email

--- tardigrada/src/backend/validation.js
+++ tardigrada/src/backend/validation.js
@@ -2,6 +2,7 @@ const { check } = require('express-validator');
 
 exports.signupValidation = [
     check('name', 'Name should have at least 3 letters').isLength({ min: 3 }),
+    check('email', 'Please include a valid email').isEmail(),
     check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
 ]
 
