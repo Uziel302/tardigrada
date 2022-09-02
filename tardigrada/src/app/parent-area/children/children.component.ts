a2f60fc94feec052adf3a6655572615258e91dff
Asaf M <302302302v@gmail.com>
2022-09-02 15:30:28 +0300

    formatter

--- tardigrada/src/app/parent-area/children/children.component.ts
+++ tardigrada/src/app/parent-area/children/children.component.ts
@@ -2,20 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
-
 import { IChild } from 'src/app/models/child';
 
 @Component({
   selector: 'app-children',
   templateUrl: './children.component.html',
-  styleUrls: ['./children.component.css']
+  styleUrls: ['./children.component.css'],
 })
 export class ChildrenComponent implements OnInit {
   public children = [];
   public editMode: boolean = true;
   public saveSuccess: string = '';
   public saveError: string = '';
-  
+
   public childData: IChild = {
     firstName: '',
     lastName: '',
@@ -30,18 +29,15 @@ export class ChildrenComponent implements OnInit {
     limits: '',
     telegram: '',
     note: '',
-};
+  };
 
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  saveChild(){
+  saveChild() {
     this.http
-      .post<{message: string}>(
+      .post<{ message: string }>(
         environment.apiEndPoint + 'saveChild',
         this.childData
       )
@@ -63,13 +59,11 @@ export class ChildrenComponent implements OnInit {
             limits: '',
             telegram: '',
             note: '',
-        };
+          };
         },
         (error) => {
-          this.saveError = error.message;    
+          this.saveError = error.message;
         }
-    );
+      );
   }
-
-
 }
