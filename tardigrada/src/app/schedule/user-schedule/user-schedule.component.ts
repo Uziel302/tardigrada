d59c2bc70ea9002c234af98890673f26d89c8288
Asaf M <302302302v@gmail.com>
2022-09-03 17:52:18 +0300

    hour column

--- tardigrada/src/app/schedule/user-schedule/user-schedule.component.ts
+++ tardigrada/src/app/schedule/user-schedule/user-schedule.component.ts
@@ -9,6 +9,21 @@ import { ScheduleService } from '../schedule.service';
 })
 export class UserScheduleComponent implements OnInit {
   public week: string[] = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  public hourColumn: string[] = [
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00'
+  ];
   @Input() userId: number = 0;
 
   constructor(
