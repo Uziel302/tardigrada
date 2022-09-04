1f18c6c91cd9e0af6b773bb236cb5ff6b8f25add
Asaf M <302302302v@gmail.com>
2022-09-04 14:10:13 +0300

    make validation work

--- tardigrada/src/backend/validation.js
+++ tardigrada/src/backend/validation.js
@@ -1,12 +1,34 @@
-const { check } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 
 exports.signupValidation = [
-    check('name', 'Name should have at least 3 letters').isLength({ min: 3 }),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
-]
+  check("name", "Name should have at least 3 letters").isLength({ min: 3 }),
+  check("email", "Please include a valid email").isEmail(),
+  check("password", "Password must be 6 or more characters").isLength({
+    min: 6,
+  }),
+
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(401).json({
+        message: errors.errors[0].msg,
+      });
+    }
+  },
+];
 
 exports.loginValidation = [
-     check('name', 'Name should have at least 3 letters').isLength({ min: 3 }),
-     check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
-]
+  check("name", "Name should have at least 3 letters").isLength({ min: 3 }),
+  check("password", "Password must be 6 or more characters").isLength({
+    min: 6,
+  }),
+
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(401).json({
+        message: errors.errors[0].msg,
+      });
+    }
+  },
+];
