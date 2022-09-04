d63fda8324b019cdfbc04aec8f9d5ea47a5a1fbf
Asaf M <302302302v@gmail.com>
2022-09-04 19:34:56 +0300

    working api that saves to notion

--- tardigrada/src/backend/controllers/studentData.controller.js
+++ tardigrada/src/backend/controllers/studentData.controller.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const knex = require('../dbConnection');
 const tableName = 'children';
+const { Client } = require("@notionhq/client");
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
 exports.saveChild = async (req, res) => {
+  notion.pages.create({
+    parent: { database_id: process.env.NOTION_DATABASE_ID },
+    properties: {
+      Name: {
+        title: [{ type: "text", text: { content: 'ff' } }],
+      },
+      "Issue Number": {
+        number: 3,
+      },
+      State: {
+        select: { name: 'eee' },
+      },
+      "Number of Comments": {
+        number: 33,
+      },
+      "Issue URL": {
+        url: 'http://google.com',
+      },
+    },
+  });
   knex(tableName)
   .insert(req.body)
   .then((row) => {
