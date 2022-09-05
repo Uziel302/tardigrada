b689d452c9cd71c0dbfbd7613552491c06cf883e
Asaf M <302302302v@gmail.com>
2022-09-05 22:35:06 +0300

    first working upload system

--- tardigrada/src/backend/router.js
+++ tardigrada/src/backend/router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fileUpload = require("express-fileupload");
 const { signupAndValidation, loginAndValidation } = require('./validation');
 const { sendFreeRequest } = require('./controllers/admin-emails.controller');
 const { saveChild } = require('./controllers/studentData.controller');
@@ -13,4 +14,13 @@ router.post('/api/saveChild', (req, res) => {
     return saveChild(req, res); 
   }
 });
+router.use(fileUpload());
+router.post("/upload", (req, res) => {
+    let upfile = req.files.upfile,
+        updest = __dirname + "/uploads/" + upfile.name;
+    upfile.mv(updest, (err) => {
+      if (err) { return res.status(500).send(err); }
+      res.send("File uploaded!");
+    });
+  });
 module.exports = router;
