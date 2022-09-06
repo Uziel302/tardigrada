e406b2a5c8ed54d5c9f5ae9f9ed834807b8e05fd
Asaf M <302302302v@gmail.com>
2022-09-06 12:36:30 +0300

    now sync with notion works

--- tardigrada/src/backend/controllers/studentData.controller.js
+++ tardigrada/src/backend/controllers/studentData.controller.js
@@ -1,7 +1,58 @@
 const knex = require('../dbConnection');
 const tableName = 'children';
+const { Client } = require("@notionhq/client");
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
 exports.saveChild = async (req, res) => {
+  try {
+    notion.pages.create({
+      parent: { database_id: process.env.NOTION_DATABASE_ID },
+      properties: {
+        firstName: {
+          title: [{ type: "text", text: { content: req.body.firstName } }],
+        },
+        lastName: {
+          "rich_text": [{"text": {"content": req.body.lastName}}]
+        },
+        fatherName: {
+          "rich_text": [{"text": {"content": req.body.fatherName}}]
+        },
+        dateOfBirth: {
+          "rich_text": [{"text": {"content": req.body.dateOfBirth}}]
+        },
+        className: {
+          "rich_text": [{"text": {"content": req.body.className}}]
+        },
+        subjects: {
+          "rich_text": [{"text": {"content": req.body.subjects}}]
+        },
+        timezone: {
+          "rich_text": [{"text": {"content": req.body.timezone}}]
+        },
+        zoom: {
+          "rich_text": [{"text": {"content": req.body.zoom}}]
+        },
+        device: {
+          "rich_text": [{"text": {"content": req.body.device}}]
+        },
+        language: {
+          "rich_text": [{"text": {"content": req.body.language}}]
+        },
+        limits: {
+          "rich_text": [{"text": {"content": req.body.limits}}]
+        },
+        telegram: {
+          "rich_text": [{"text": {"content": req.body.telegram}}]
+        },
+        note: {
+          "rich_text": [{"text": {"content": req.body.note}}]
+        },
+      },
+    });
+  } catch(error) {
+    res.status(401).send({ message: "Sending details to notion failed!" });
+    return false;
+  }
   knex(tableName)
   .insert(req.body)
   .then((row) => {
