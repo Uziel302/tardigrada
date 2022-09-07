933da973aaef7d39435b1da79d973fbff8ed07ec
Asaf M <302302302v@gmail.com>
2022-09-07 23:36:45 +0300

    first commit of teacher area

--- tardigrada/src/app/teacher-area/teacher-area.component.ts
+++ tardigrada/src/app/teacher-area/teacher-area.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ScheduleService } from '../schedule/schedule.service';
+import { LoginService } from '../login-screen/login.service';
+
 @Component({
   selector: 'app-teacher-area',
   templateUrl: './teacher-area.component.html',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherAreaComponent implements OnInit {
 
-  constructor() { }
+  public userId: number = 0;
+  public showSettings: boolean = false;
+  public cover: string = '';
+  public profile: string = '';
+
+  constructor(
+    public scheduleService: ScheduleService,
+    public loginService: LoginService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  stopProp(event: any){
+    event.stopPropagation();
+  }
+
+  changeCover (event: string) {
+    this.cover = event;
+  }
+
+  changeProfile (event: string) {
+    this.profile = event;
   }
 
 }
