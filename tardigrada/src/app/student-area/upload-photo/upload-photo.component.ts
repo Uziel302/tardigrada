e7c11dfdb75acc76c9737ab4bbc98c1101c6413b
Asaf M <302302302v@gmail.com>
2022-09-11 10:39:20 +0300

    saveNoteWithFile

--- tardigrada/src/app/student-area/upload-photo/upload-photo.component.ts
+++ tardigrada/src/app/student-area/upload-photo/upload-photo.component.ts
@@ -4,47 +4,48 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { LoginService } from 'src/app/login-screen/login.service';
 
-
 @Component({
   selector: 'app-upload-photo',
   templateUrl: './upload-photo.component.html',
-  styleUrls: ['./upload-photo.component.css']
+  styleUrls: ['./upload-photo.component.css'],
 })
 export class UploadPhotoComponent {
-    public fileName = '';
-    @Input() uploadType: string = '';
-    @Input() isStudent: boolean = true;
+  public fileName = '';
+  @Input() uploadColumn: string = '';
+  @Input() uploadTable: string = '';
+  @Input() additionalColumn: string = '';
+  @Input() additionalContent: string = '';
 
-    constructor(
-      private http: HttpClient,
-      public loginService: LoginService,
-    ) {}
+  constructor(private http: HttpClient, public loginService: LoginService) {}
 
-    onFileSelected(event: any) {
-        const file: File = event.target.files[0];
-        if (file) {
-            this.fileName = file.name;
-            const formData = new FormData();
-            formData.append("upfile", file);
-            formData.append("childId", ''+this.loginService.currentChildId);
-            formData.append("uploadType", this.uploadType);
-            formData.append("tableName", this.isStudent ? 'children' : 'teachers');
-            formData.append("uploadsFolder", environment.uploadsFolder);
-            const upload$ = this.http.post<{filename: string}>(environment.apiEndPoint + 'upload', formData);
-            upload$.subscribe(
-              (data) => {
-                //this timeout is needed to prevent trying access the photo that was just saved before it's ready
-                setTimeout(() => {
-                  const currentUser = this.isStudent ? 'currentChild' : 'teacher';
-                  if(this.uploadType === 'cover'){
-                    this.loginService[currentUser].cover = data.filename;
-                  }
-                  if(this.uploadType === 'profile'){
-                    this.loginService[currentUser].profile = data.filename;
-                  }
-                }, 5)
-              }
-            );
-        }
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.fileName = file.name;
+      const formData = new FormData();
+      formData.append('upfile', file);
+      formData.append('childId', '' + this.loginService.currentChildId);
+      formData.append('uploadColumn', this.uploadColumn);
+      formData.append('uploadTable', this.uploadTable);
+      formData.append('uploadsFolder', environment.uploadsFolder);
+      formData.append('additionalColumn', this.additionalColumn);
+      formData.append('additionalContent', this.additionalContent);
+      const upload$ = this.http.post<{ filename: string }>(
+        environment.apiEndPoint + 'upload',
+        formData
+      );
+      upload$.subscribe((data) => {
+        //this timeout is needed to prevent trying access the photo that was just saved before it's ready
+        setTimeout(() => {
+          const currentUser = this.uploadTable === 'children' ? 'currentChild' : 'teacher';
+          if (this.uploadColumn === 'cover') {
+            this.loginService[currentUser].cover = data.filename;
+          }
+          if (this.uploadColumn === 'profile') {
+            this.loginService[currentUser].profile = data.filename;
+          }
+        }, 5);
+      });
     }
+  }
 }
--- tardigrada/src/backend/controllers/upload.controller.js
+++ tardigrada/src/backend/controllers/upload.controller.js
@@ -1,21 +1,37 @@
-const knex = require('../dbConnection');
+const knex = require("../dbConnection");
 
 exports.saveToDb = async (req, res, filename) => {
   const newData = {};
-  newData[req.body.uploadType] = req.body.uploadsFolder + filename;
-  const whereObj =  {};
-  if(req.body.tableName === 'children'){
+  newData[req.body.uploadColumn] = req.body.uploadsFolder + filename;
+  const whereObj = {};
+  if (req.body.uploadTable === "children") {
     whereObj.id = req.body.childId;
-  } else if(req.body.tableName === 'teachers'){
+  } else if (req.body.uploadTable === "teachers") {
     whereObj.userId = req.body.userId;
+  } else if (req.body.uploadTable === "teachersNotes") {
+    return this.saveNoteWithFile(req, res, filename);
   } else {
-    return res.status(500).send({error: 'wrong table name'})
+    return res.status(500).send({ error: "wrong table name" });
   }
 
-  knex(req.body.tableName)
-  .update(newData)
-  .where(whereObj)
-  .then(u => res.status(!!u?200:404).send({filename: req.body.uploadsFolder + filename}))
+  knex(req.body.uploadTable)
+    .update(newData)
+    .where(whereObj)
+    .then((u) =>
+      res
+        .status(!!u ? 200 : 404)
+        .send({ filename: req.body.uploadsFolder + filename })
+    )
+    .catch((e) => res.status(500).json(e));
+};
+
+exports.saveNoteWithFile = async (req, res, filename) => {
+  const newData = {};
+  newData[req.body.uploadColumn] = req.body.uploadsFolder + filename;
+  newData[req.body.additionalColumn] = req.body.additionalContent;
+  knex(req.body.uploadTable)
+  .insert(newData)
+  .then(u => res.status(!!u?200:404).json({id:u[0]}))
   .catch(e => res.status(500).json(e));
 }
 
@@ -24,7 +40,9 @@ exports.upload = async (req, res) => {
   const filename = Date.now() + upfile.name;
   const updest = __dirname + "/../../assets/uploads/" + filename;
   upfile.mv(updest, (err) => {
-    if (err) { return res.status(500).send(err); }
+    if (err) {
+      return res.status(500).send(err);
+    }
     this.saveToDb(req, res, filename);
   });
-}
+};
