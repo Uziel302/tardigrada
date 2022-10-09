af6ace13ab24a6c67d9cd9c985597a4e88159a46
Asaf M <302302302v@gmail.com>
2022-10-09 19:36:56 +0300

    checkedAges

--- tardigrada/src/app/schedule/schedule.component.ts
+++ tardigrada/src/app/schedule/schedule.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { ILecture } from '../models/lecture';
 
 import { ScheduleService } from '../schedule/schedule.service';
 
@@ -49,9 +50,8 @@ export class ScheduleComponent implements OnInit, OnDestroy {
   public ageList: number[] = [
     4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
   ];
-  public checkedAges: number[] = [...this.ageList];
+  public checkedAges: number[] = [];
   public search: string = '';
-
 
   private subscriptions: Subscription[] = [];
 
@@ -59,7 +59,7 @@ export class ScheduleComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.calculateWeekDates();
-    
+
     this.subscriptions.push(
       this.scheduleService.getLecturesData().subscribe((lecturesData: any) => {
         this.scheduleService.processLecturesData(lecturesData);
@@ -73,7 +73,19 @@ export class ScheduleComponent implements OnInit, OnDestroy {
     }
   }
 
-  checkboxChanged(age: number) {
+  calculateWeekDates() {
+    let weekStart = new Date(
+      new Date().setDate(new Date().getDate() - new Date().getDay())
+    );
+    for (let weekDay of this.weekDays) {
+      let date = new Date(
+        new Date(weekStart.getTime()).setDate(weekStart.getDate() + weekDay)
+      );
+      this.weekDates.push(date.getDate() + ' ' + this.months[date.getMonth()]);
+    }
+  }
+
+  toggleAge(age: number) {
     if (this.checkedAges.includes(age)) {
       this.checkedAges.splice(this.checkedAges.indexOf(age), 1);
     } else {
@@ -81,11 +93,17 @@ export class ScheduleComponent implements OnInit, OnDestroy {
     }
   }
 
-  calculateWeekDates(){
-    let weekStart = new Date((new Date()).setDate((new Date()).getDate()-(new Date()).getDay()));
-    for(let weekDay of this.weekDays){
-      let date = new Date((new Date(weekStart.getTime())).setDate(weekStart.getDate()+weekDay));
-      this.weekDates.push(date.getDate()+' '+this.months[date.getMonth()]);
+  filterAge(lecture: ILecture):boolean {
+    if(!this.checkedAges.length){
+      return true;
     }
+
+    for(let age of this.checkedAges){
+      if(age <= lecture.maxAge && age >= lecture.minAge){
+        return true;
+      }
+    }
+    
+    return false;
   }
 }
