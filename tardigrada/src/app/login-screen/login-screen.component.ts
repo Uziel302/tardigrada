0f161cfd05b4339877a1a9c1c559c401d28e9597
Asaf M <302302302v@gmail.com>
2023-02-24 18:36:26 +0200

    remove school password

--- tardigrada/src/app/login-screen/login-screen.component.ts
+++ tardigrada/src/app/login-screen/login-screen.component.ts
@@ -16,7 +16,6 @@ export class LoginScreenComponent implements OnInit {
   public email: string = '';
   public telegram: string = '';
   public password: string = '';
-  public schoolPassword: string = '';
   public teacherId: number = 0;
 
   constructor(
@@ -38,8 +37,6 @@ export class LoginScreenComponent implements OnInit {
       Object.keys(formObj.form.controls).forEach((key) => {
         formObj.controls[key].markAsTouched();
       });
-    } else if(this.teacherId && this.schoolPassword !== 'Tihohodka1') {
-      return;
     } else {
       this.loginService.createUser(
         this.firstName,
