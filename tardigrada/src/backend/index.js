de404b134b9d13bb0b430ad4db2b4a0b5e41cd81
Asaf M <302302302v@gmail.com>
2023-08-04 18:21:16 +0300

    Revert "{     origin: '*' }"
    This reverts commit d2c5471638057cebffc619de87950a233c019e16.

--- tardigrada/src/backend/index.js
+++ tardigrada/src/backend/index.js
@@ -4,9 +4,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const indexRouter = require("./router.js");
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 
